9030a692be811ed2b2baafc2462df93a65588ce9
valentyna <chepurnavalentyna@gmail.com>
2024-08-12 16:02:54 +0300

    first corrected

--- app/src/main/java/com/project/myapp/MainActivity.kt
+++ app/src/main/java/com/project/myapp/MainActivity.kt
@@ -10,9 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
