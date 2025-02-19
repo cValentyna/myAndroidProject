984ad6ae893840e1ca29a5c223a7b002e4004efd
valentyna <chepurnavalentyna@gmail.com>
2025-02-19 22:37:03 +0200

    level1 - rewrote AuthState as sealed class

--- app/src/main/java/com/project/myapp/screens/auth/AuthState.kt
+++ app/src/main/java/com/project/myapp/screens/auth/AuthState.kt
@@ -1,14 +1,25 @@
 package com.project.myapp.screens.auth
 
 /**
- * Class where information about the state is stored
+ * Class where ui state is stored
  */
-data class AuthState(
-    var isUserEmailValid: Boolean = false,
-    var isUserPasswordValid: Boolean = false,
-    var wasFocusEmail: Boolean = false,
-    var firstFocusEmail: Boolean = false,
-    var wasFocusPassword: Boolean = false,
-    var firstFocusPassword: Boolean = false,
-    var wasRegisterButtonClicked: Boolean = false,
-)
+
+sealed class AuthState {
+    data object Valid : AuthState()
+    data class Error(val message: Int?) : AuthState()
+
+    sealed class EmailState {
+        data object Valid : EmailState()
+        data class Error(val message: Int?) : EmailState()
+        data class Empty(val message: Int?) : EmailState()
+        data object InvisibleError : EmailState()
+    }
+
+    sealed class PasswordState {
+        data object Valid : PasswordState()
+        data class Error(val message: Int?) : PasswordState()
+        data class Empty(val message: Int?) : PasswordState()
+        data object InvisibleError : PasswordState()
+    }
+}
+
