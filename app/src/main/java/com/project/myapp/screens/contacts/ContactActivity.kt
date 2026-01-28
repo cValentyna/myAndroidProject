33755f6834d3171f0905d9b605dcbd4cd6acd4fa
valentyna <chepurnavalentyna@gmail.com>
2026-01-28 17:49:51 +0200

    level 2 part: added swipe to delete

--- app/src/main/java/com/project/myapp/screens/contacts/ContactActivity.kt
+++ app/src/main/java/com/project/myapp/screens/contacts/ContactActivity.kt
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.project.myapp.R
 import com.project.myapp.data.contacts.User
 import com.project.myapp.databinding.ActivityContactsBinding
@@ -40,6 +42,7 @@ class ContactActivity : AppCompatActivity() {
         setView()
         initRecyclerView()
         collectUserList()
+        swipeToDelete()
     }
 
     private fun setView() {
@@ -87,5 +90,30 @@ class ContactActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun swipeToDelete() {
+        ItemTouchHelper(
+            object : ItemTouchHelper.SimpleCallback(
+                0,
+                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT,
+            ) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder,
+                ): Boolean = false
+
+                override fun onSwiped(
+                    viewHolder: RecyclerView.ViewHolder,
+                    direction: Int,
+                ) {
+                    val position = viewHolder.adapterPosition
+                    val item = contactsAdapter.currentList[position]
+                    viewModel.deleteUser(item)
+                    showUndoSnackbar(item)
+                }
+            },
+        ).attachToRecyclerView(binding.recyclerViewContacts)
     }
 }
