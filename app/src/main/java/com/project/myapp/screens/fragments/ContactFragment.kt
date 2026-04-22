9764d25e6704c188ddc85bf4f38af8dc9b822007
valentyna <chepurnavalentyna@gmail.com>
2026-04-22 15:19:02 +0300

    Level 3 moved contacts list markup to a fragment

--- app/src/main/java/com/project/myapp/screens/fragments/ContactFragment.kt
+++ app/src/main/java/com/project/myapp/screens/fragments/ContactFragment.kt
@@ -1,8 +1,11 @@
-package com.project.myapp.screens.contacts
+package com.project.myapp.screens.fragments
 
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -11,67 +14,78 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.project.myapp.R
 import com.project.myapp.data.contacts.User
-import com.project.myapp.databinding.ActivityContactsBinding
-import com.project.myapp.ext.componentactivity.enableEdgeToEdgeGrayStatusBar
-import com.project.myapp.ext.view.initializeWindowInsetsHandling
+import com.project.myapp.databinding.FragmentContactBinding
 import com.project.myapp.ext.view.snackBar
 import com.project.myapp.imageloader.ImageLoader
+import com.project.myapp.screens.contacts.ContactAdapter
+import com.project.myapp.screens.contacts.ContactDialogFragment
 import com.project.myapp.screens.contacts.ContactDialogFragment.Companion.KEY_NAME
 import com.project.myapp.screens.contacts.ContactDialogFragment.Companion.KEY_PROFESSION
 import com.project.myapp.screens.contacts.ContactDialogFragment.Companion.REQUEST_KEY
+import com.project.myapp.screens.contacts.ContactViewModel
+import com.project.myapp.screens.contacts.ItemDecorator
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class ContactActivity : AppCompatActivity() {
-    private val binding: ActivityContactsBinding by lazy {
-        ActivityContactsBinding.inflate(layoutInflater)
-    }
+class ContactFragment : Fragment(R.layout.fragment_contact) {
+    private var _binding: FragmentContactBinding? = null
+    private val binding get() = _binding!!
+
     private val viewModel: ContactViewModel by viewModels()
 
     @Inject
     lateinit var imageLoader: ImageLoader
     private val contactsAdapter by lazy {
-        ContactAdapter(imageLoader) { user ->
-            viewModel.deleteUser(user)
-            val deletedUser = user
-            showUndoSnackbar(deletedUser)
-        }
+        ContactAdapter(
+            imageLoader,
+            onDeleteUser = { user ->
+                viewModel.deleteUser(user)
+                val deletedUser = user
+                showUndoSnackbar(deletedUser)
+            },
+        )
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setView()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentContactBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         initRecyclerView()
         collectUserList()
         swipeToDelete()
         setupAddContactDialog()
     }
 
-    private fun setView() {
-        enableEdgeToEdgeGrayStatusBar()
-        setContentView(binding.root)
-        binding.root.initializeWindowInsetsHandling()
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     private fun initRecyclerView() =
         with(binding.recyclerViewContacts) {
-            addItemDecoration(ItemDecorator(resources.getDimensionPixelSize(R.dimen.gap_item)))
-            layoutManager = LinearLayoutManager(this@ContactActivity)
+            addItemDecoration(ItemDecorator(resources.getDimensionPixelSize(com.project.myapp.R.dimen.gap_item)))
+            layoutManager = LinearLayoutManager(requireContext())
             adapter = contactsAdapter
-            itemAnimator = null
         }
 
     private fun collectUserList() {
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.userList.collectLatest { users ->
-                    contactsAdapter.update(users) {
+                viewModel.userList
+                    .collectLatest { users ->
+                        contactsAdapter.update(users)
                         scrollToRestorePosition()
                     }
-                }
             }
         }
     }
@@ -80,19 +94,28 @@ class ContactActivity : AppCompatActivity() {
       Scrolls RecyclerView to the restored user if it was last or first.
       Uses "post" to make sure LayoutManager position are updated after
       the list has been drawn again.
+      added addOnLayoutChangeListener
      */
 
     private fun scrollToRestorePosition() {
-        val restoredIndex = viewModel.getLastRestoredIndex() ?: return
-        binding.recyclerViewContacts.post {
-            val layoutManager =
-                binding.recyclerViewContacts.layoutManager as LinearLayoutManager
-            val firstVisible = layoutManager.findFirstVisibleItemPosition()
-            val lastVisible = layoutManager.findLastVisibleItemPosition()
-            if (restoredIndex < firstVisible || restoredIndex > lastVisible) {
-                binding.recyclerViewContacts.smoothScrollToPosition(restoredIndex)
+        binding.recyclerViewContacts.addOnLayoutChangeListener(object : View.OnLayoutChangeListener {
+            override fun onLayoutChange(
+                v: View?, left: Int, top: Int, right: Int, bottom: Int,
+                oldLeft: Int, oldTop: Int, oldRight: Int, oldBottom: Int,
+            ) {
+                val restoredIndex = viewModel.getLastRestoredIndex() ?: return
+                binding.recyclerViewContacts.post {
+                    val layoutManager =
+                        binding.recyclerViewContacts.layoutManager as LinearLayoutManager
+                    val firstVisible = layoutManager.findFirstVisibleItemPosition()
+                    val lastVisible = layoutManager.findLastVisibleItemPosition()
+                    if (restoredIndex < firstVisible || restoredIndex > lastVisible) {
+                        binding.recyclerViewContacts.smoothScrollToPosition(restoredIndex)
+                    }
+                }
+                binding.recyclerViewContacts.removeOnLayoutChangeListener(this)
             }
-        }
+        })
     }
 
     private fun showUndoSnackbar(deletedUser: User) {
@@ -135,13 +158,13 @@ class ContactActivity : AppCompatActivity() {
     private fun setupAddContactDialog() {
         binding.contactAddContacts.setOnClickListener {
             val dialog = ContactDialogFragment()
-            dialog.show(supportFragmentManager, "customDialog")
+            dialog.show(childFragmentManager, "customDialog")
         }
         setAddUserResultListener()
     }
 
     private fun setAddUserResultListener() {
-        supportFragmentManager.setFragmentResultListener(REQUEST_KEY, this) { _, bundle ->
+        childFragmentManager.setFragmentResultListener(REQUEST_KEY, this) { _, bundle ->
             val name = bundle.getString(KEY_NAME)
             val profession = bundle.getString(KEY_PROFESSION)
             viewModel.addUser(name.toString(), profession.toString())
